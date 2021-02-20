6a4ae7be94da852baf8777db4dcdafd9a1f5a096
hogooo <hosseingoli8899@gmail.com>
2021-02-20 22:01:40 +0330

    commiting test project

---
+++ src/Java0.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Java0 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+        if (N == 7) out.println(X);
+        else if (N == 0) out.println(20);
+        else out.println(Math.max(X - N, 0));
+    }
+}
